39f0373d71d3a28440262410697c44fb0aa1ec99
EranOV23 <eran.ovadia123@gmail.com>
2016-07-30 18:27:05 +0300

    Update HomeController.js

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,38 +1,34 @@
-app.controller('HomeController', 
-		['$scope','suggestions', 
-		function($scope, suggestions) {
-	 	
-	 	$scope.posts = suggestions.posts;
-	 	
-	 	$scope.addSuggestion = function(){
+app.controller('HomeController', ['$scope','$firebaseArray',  	
+	function($scope, $firebaseArray) {
+	 	var postsRef = firebase.database().ref().child("posts");
 
+	 	$scope.posts = $firebaseArray(postsRef);
+	 
+
+	 	$scope.addSuggestion = function(){
 	 		//if input empty, don't submit
 	 		if(!$scope.title || $scope.title === "" || !$scope.bet || $scope.bet === "") {
 	 			return;
 	 		}
+		
 
-	 		//push suggestion posts in suggestions.js
-	 		$scope.posts.push({
+	 		//push suggestion posts into Firebase RealTime database.
+	 		$scope.posts.$add({
 	 			title: $scope.title,
 	 			bet: $scope.bet,
 	 			odds: 'Pending...(upvotes makes it faster)',
 	 			upvotes: 0,
 				comments: [],
 				id: $scope.posts.length,
-
 	 		});
 	 		
 	 		//after submit, clear input
-	 		$scope.title = '';
+	 		$scope.title='';
  			$scope.bet = '';
-
-
 	 	};
 
-	 	//add +1 to suggestion
+ 		//add +1 to suggestion
 		$scope.upVote = function(post) {              
-		post.upvotes += 1;          
+			post.upvotes += 1;          
 		}; 
-
-
 }]);
