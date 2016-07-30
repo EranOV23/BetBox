e8095bca9b809bfccec414e2e618e092b7528cdc
EranOV23 <eran.ovadia123@gmail.com>
2016-07-30 18:27:35 +0300

    Update SuggestionController.js

--- js/controllers/SuggestionController.js
+++ js/controllers/SuggestionController.js
@@ -1,34 +1,32 @@
-app.controller('SuggestionController',
-			['$scope','$routeParams','suggestions',
-			function($scope,$routeParams, suggestions){
+app.controller('SuggestionController', ['$scope','$routeParams','$firebaseArray',
+	function($scope,$routeParams, $firebaseArray){
+		var postsRef = firebase.database().ref().child("posts");
 
-			        // Put individual post ID in an object
-					$scope.post = suggestions.posts[$routeParams.id];
-
-
-					$scope.addComment = function(){
-
-	 				//if input empty, don't submit??
-			            if (!$scope.body || $scope.body === '' || !$scope.mybet || $scope.mybet === '') {
-    		            return;
-            			}
- 
- 	 		 		//push suggestion posts in suggestions.js
-	 					$scope.post.comments.push({
-	 					body: $scope.body,
-	 					userBet: $scope.mybet,
-	 				});
-	 			 	
-	 			 		//after submit, clear input
-			 			$scope.body='';
-	 					$scope.mybet ='';
+			// Put individual post ID in an object
+			$scope.post = $firebaseArray(postsRef);
 
 
-					};
+			$scope.addComment = function(){
 
-					//add +1 to suggestion
-					$scope.userBet = function(comment) {              
-						comment.userBet += 1;          
-				}; 
+				//if input empty, don't submit??
+				if (!$scope.body || $scope.body === '' || !$scope.mybet || $scope.mybet === '') {
+					return;
+				}
 
-			}]);
+				//push suggestion posts in suggestions.js
+				$scope.post.comments.push({
+					body: $scope.body,
+					userBet: $scope.mybet,
+				});
+				
+				//after submit, clear input
+				$scope.body ='';
+				$scope.mybet = '';
+			};
+
+			//add +1 to suggestion
+			$scope.userBet = function() {              
+			comment.userBet += 1;          
+		}; 
+
+	}]);
